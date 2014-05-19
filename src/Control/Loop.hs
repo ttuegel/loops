17d8fc8a5800303d2d5e373613eacc34d403d259
Thomas Tuegel <ttuegel@gmail.com>
2014-05-19 15:17:41 +0000

    Add continue

--- src/Control/Loop.hs
+++ src/Control/Loop.hs
@@ -3,6 +3,7 @@
 
 module Control.Loop
     ( Loop, LoopT, LoopPrim(..)
+    , continue
     , for, unfoldl
     , module Control.Monad.Trans.Class
     ) where
@@ -14,12 +15,13 @@ import Data.Foldable
 import Data.Maybe (fromJust, isJust)
 import Prelude hiding (foldr)
 
-data LoopPrim a = forall b. For b (b -> Bool) (b -> b) (b -> a)
+data LoopPrim a = forall b. For b (b -> Bool) (b -> b) (b -> a) | Continue
 
 instance Functor LoopPrim where
     fmap f prim =
       case prim of
         For i0 check next g -> For i0 check next (f . g)
+        Continue -> Continue
     {-# INLINE fmap #-}
 
 instance Foldable LoopPrim where
@@ -29,6 +31,7 @@ instance Foldable LoopPrim where
           let _for i | check i = f (g i) $ _for $ next i
                      | otherwise = r0
           in _for i0
+        Continue -> r0
     {-# INLINE foldr #-}
 
     foldl' f r0 prim =
@@ -41,6 +44,7 @@ instance Foldable LoopPrim where
                   in i' `seq` r' `seq` _for i' r'
                 | otherwise = r
           in _for i0 r0
+        Continue -> r0
     {-# INLINE foldl' #-}
 
 type Loop = F LoopPrim
@@ -53,3 +57,7 @@ for i0 check next = liftF $ For i0 check next id
 unfoldl :: (Functor m, MonadFree LoopPrim m) => (i -> Maybe (i, r)) -> i -> m r
 unfoldl unf i0 = fmap (fromJust . fmap snd) $ for (unf i0) isJust (>>= unf . fst)
 {-# INLINE unfoldl #-}
+
+continue :: MonadFree LoopPrim m => m r
+continue = liftF $ Continue
+{-# INLINE continue #-}
