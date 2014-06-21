778890a62ce39af0ab339b55e188060c147a6267
Thomas Tuegel <ttuegel@gmail.com>
2014-06-21 09:46:00 -0500

    Rewrite numFromNU in terms of Alternative

--- src/Control/Monad/Loop/Unroll.hs
+++ src/Control/Monad/Loop/Unroll.hs
@@ -1,5 +1,6 @@
 module Control.Monad.Loop.Unroll where
 
+import Control.Applicative
 import GHC.TypeLits
 
 import Control.Monad.Loop.Internal
@@ -9,11 +10,12 @@ numFromNU
     :: (Integral a, KnownNat n, Num a, Ord a, Unrolling n)
     => Static n -> a -> a -> LoopR r m a
 {-# INLINE numFromNU #-}
-numFromNU unr = \a0 n -> do
-    let bigStep = fromIntegral $ natVal unr
-        an = a0 + n
-        bigStop = a0 + (n `div` bigStep) * bigStep
-    i <- for a0 (< an) (+ bigStep)
-    if i < bigStop
-      then iterateS unr i (+ 1)
-      else for i (< an) (+ 1)
+numFromNU unr = \a0 n ->
+    let step = fromIntegral $ natVal unr
+        end = a0 + n
+        bigEnd = end - (n `mod` step)
+        big = do
+            i <- for a0 (< bigEnd) (+ step)
+            iterateS unr i (+ 1)
+        little = for bigEnd (< end) (+ 1)
+    in big <|> little
