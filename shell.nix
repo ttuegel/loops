2a7dff7332e9b2b4a7ff499ad0d7dbd951760532
Thomas Tuegel <ttuegel@gmail.com>
2014-08-12 20:24:00 -0500

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,6 @@
-{ pkgs ? (import <nixpkgs> {}), haskellPackages ? pkgs.haskellPackages_ghc782
-, hsDevTools ? pkgs.hsDevTools or null }:
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages
+, hsDevTools ? pkgs.hsDevTools or null
+}:
 
-let inherit (haskellPackages)
-      cabal criterion primitive tasty tastyQuickcheck transformers vector;
-in
-cabal.mkDerivation (self: {
-  pname = "free-loops";
-  version = "0.2.0.2";
-  src = ./.;
-  buildDepends = [ primitive transformers vector ];
-  testDepends = [ criterion tasty tastyQuickcheck ];
-  buildTools = if hsDevTools == null then [] else hsDevTools haskellPackages;
-  hyperlinkSource = true;
-  meta = {
-    description = "Fast imperative-style loops";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+haskellPackages.callPackage ./default.nix {}
