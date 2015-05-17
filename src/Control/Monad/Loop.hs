96fad8dd31ea346cc605dbea0835c023d631871f
Thomas Tuegel <ttuegel@gmail.com>
2015-05-17 12:44:37 -0500

    Clean up instances

--- src/Control/Monad/Loop.hs
+++ src/Control/Monad/Loop.hs
@@ -22,54 +22,36 @@ newtype Loop a = Loop { runLoop :: forall r.
                       }
 
 instance Functor Loop where
-  {-# INLINE [1] fmap #-}
-  fmap f as = Loop fmap_loop where
-    {-# INLINE [0] fmap_loop #-}
-    fmap_loop y r = runLoop as yieldA r where
-      yieldA =
-        case y of
-          Left yieldB -> Left $ \s a -> yieldB s (f a)
-          Right yieldB -> Right $ \a s -> yieldB (f a) s
+  {-# INLINE fmap #-}
+  fmap = \f as ->
+    let fmap_yield_left = \yield r a -> yield r (f a)
+        fmap_yield_right = \yield a r -> yield (f a) r
+    in Loop $ \y -> runLoop as $ both fmap_yield_left fmap_yield_right y
 
 instance Applicative Loop where
-  {-# INLINE [1] pure #-}
-  pure a = Loop pure_loop where
-    {-# INLINE [0] pure_loop #-}
-    pure_loop y r =
-      case y of
-        Left yield -> yield r a
-        Right yield -> yield a r
+  {-# INLINE pure #-}
+  pure = \a -> Loop $ \y r ->
+    let pure_loop_left = \yield -> yield r a
+        pure_loop_right = \yield -> yield a r
+    in either pure_loop_left pure_loop_right y
 
-  {-# INLINE [1] (<*>) #-}
-  (<*>) fs as = Loop ap_loop
-    where
-      {-# INLINE [0] ap_loop #-}
-      ap_loop y r = runLoop fs yieldF r where
-        yieldF =
-          case y of
-            Left yieldB ->
-              let yieldA f = Left $ \s a -> yieldB s (f a)
-              in Left $ \s f -> runLoop as (yieldA f) s
-            Right yieldB ->
-              let yieldA f = Right $ \a s -> yieldB (f a) s
-              in Right $ \f s -> runLoop as (yieldA f) s
+  {-# INLINE (<*>) #-}
+  (<*>) = \fs as -> Loop $ \y ->
+    let ap_loop_left = \yieldB r f ->
+          runLoop as (Left $ \s a -> yieldB s (f a)) r
+        ap_loop_right = \yieldB f r ->
+          runLoop as (Right $ \a s -> yieldB (f a) s) r
+    in runLoop fs $ both ap_loop_left ap_loop_right y
 
 instance Monad Loop where
   {-# INLINE return #-}
   return = pure
 
   {-# INLINE [1] (>>=) #-}
-  (>>=) as f = Loop bind_loop
-    where
-      {-# INLINE [0] bind_loop #-}
-      bind_loop y r =
-        case y of
-          Left _ ->
-            let yieldA = Left $ \s a -> runLoop (f a) y s
-            in runLoop as yieldA r
-          Right _ ->
-            let yieldA = Right $ \a s -> runLoop (f a) y s
-            in runLoop as yieldA r
+  (>>=) = \as f -> Loop $ \y ->
+    let bind_loop_left = \_ r a -> runLoop (f a) y r
+        bind_loop_right = \_ a r -> runLoop (f a) y r
+    in runLoop as $ both bind_loop_left bind_loop_right y
 
 instance Foldable Loop where
   {-# INLINE foldr #-}
@@ -109,3 +91,7 @@ enumFromStepN :: Num a => a -> a -> Int -> Loop a
 {-# INLINE enumFromStepN #-}
 enumFromStepN !x !y !n =
   fst <$> for (x, n) (\(_, m) -> m > 0) (\(w, m) -> (w + y, m - 1))
+
+both :: (a -> c) -> (b -> d) -> Either a b -> Either c d
+{-# INLINE both #-}
+both fl fr = either (Left . fl) (Right . fr)
