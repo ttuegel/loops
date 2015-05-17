054394eb2bf818107c506724031efce33d3ae312
Thomas Tuegel <ttuegel@gmail.com>
2015-05-17 09:06:38 -0500

    Get INLINE phases right

--- src/Control/Monad/Loop.hs
+++ src/Control/Monad/Loop.hs
@@ -11,32 +11,34 @@ import Data.Foldable
 newtype Loop a = Loop { runLoop :: forall f. Applicative f => (a -> f ()) -> f () }
 
 instance Functor Loop where
-  {-# INLINE fmap #-}
+  {-# INLINE [1] fmap #-}
   fmap = \f as -> Loop (\yield -> runLoop as (yield . f))
 
 instance Applicative Loop where
-  {-# INLINE pure #-}
+  {-# INLINE [1] pure #-}
   pure = \a -> Loop (\yield -> yield a)
 
-  {-# INLINE (<*>) #-}
+  {-# INLINE [1] (<*>) #-}
   (<*>) = \fs as -> Loop (\yield -> runLoop fs (\f -> runLoop as (yield . f)))
 
 instance Monad Loop where
-  {-# INLINE return #-}
+  {-# INLINE [1] return #-}
   return = pure
 
-  {-# INLINE (>>=) #-}
+  {-# INLINE [1] (>>=) #-}
   (>>=) = \as f -> Loop (\yield -> runLoop as (\a -> runLoop (f a) yield))
 
 instance Foldable Loop where
-  {-# INLINE foldr #-}
+  {-# INLINE [1] foldr #-}
   foldr = \f r as ->
-    let foldr_go a = StL.modify (. f a)
+    let {-# INLINE [0] foldr_go #-}
+        foldr_go a = StL.modify (. f a)
     in StL.execState (runLoop as foldr_go) id r
 
-  {-# INLINE foldl' #-}
+  {-# INLINE [1] foldl' #-}
   foldl' = \f r as ->
-    let foldl'_go a = do
+    let {-# INLINE [0] foldl'_go #-}
+        foldl'_go a = do
           !s <- StS.get
           let !t = f s a
           StS.put t
@@ -47,10 +49,11 @@ empty :: Loop a
 empty = Loop (\_ -> pure ())
 
 for :: a -> (a -> Bool) -> (a -> a) -> Loop a
-{-# INLINE for #-}
+{-# INLINE [1] for #-}
 for a check next =
   Loop (\yield ->
-          let for_loop b | check b = yield b *> for_loop (next b)
+          let {-# INLINE [0] for_loop #-}
+              for_loop b | check b = yield b *> for_loop (next b)
                          | otherwise = pure ()
           in for_loop a)
 
