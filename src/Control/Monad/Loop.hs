66e93038a0ea6e31bb235f21b8a9b83328334dd3
Thomas Tuegel <ttuegel@gmail.com>
2015-05-21 19:13:50 -0500

    Implement Iter instead of Unroll

--- src/Control/Monad/Loop.hs
+++ src/Control/Monad/Loop.hs
@@ -7,7 +7,9 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Control.Monad.Loop where
 
@@ -26,6 +28,13 @@ import Prelude hiding (foldr, foldl)
 
 data Step s a = Done | Skip s | Yield a s
 
+instance Functor (Step s) where
+    {-# INLINE fmap #-}
+    fmap = \f -> \case
+      Yield a s -> Yield (f a) s
+      Skip s -> Skip s
+      Done -> Done
+
 -- | Promoted data type for tracking loop shapes.
 data LS = F        -- ^ flat loop
         | M LS     -- ^ mapped loop
@@ -39,7 +48,7 @@ data LS = F        -- ^ flat loop
 -- a smaller shape than their containing loops.
 data LoopI :: LS -> (* -> *) -> * -> * where
     Flat :: (s -> m (Step s a)) -> s -> LoopI 'F m a
-    Map :: (a -> b) -> LoopI i m a -> LoopI ('M i) m b
+    Map :: (forall s. Step s a -> m (Step s b)) -> LoopI i m a -> LoopI ('M i) m b
     Pure :: m a -> LoopI 'P m a
     Ap :: LoopI i m (a -> b) -> LoopI j m a -> LoopI ('A i j) m b
     Bind :: LoopI i m a -> (a -> Loop m b) -> LoopI ('B i) m b
@@ -51,113 +60,206 @@ data LoopI :: LS -> (* -> *) -> * -> * where
 -- containing loops, it is safe to call these class members recursively within
 -- their own implementations: the 'LS' index of the inner loop always differs,
 -- so GHC will inline all the \"recursive\" calls.
-class Unroll (n :: LS) where
-    nfoldlM :: Monad m => (a -> b -> m a) -> a -> LoopI n m b -> m a
-    nfoldlM' :: Monad m => (a -> b -> m a) -> a -> LoopI n m b -> m a
-    nfoldrM :: Monad m => (a -> b -> m b) -> b -> LoopI n m a -> m b
+class Iter (n :: LS) where
+    iterl :: Monad m => LoopI n m a -> (forall s. r -> Step s a -> m (Step s r)) -> r -> m r
+    iterl' :: Monad m => LoopI n m a -> (forall s. r -> Step s a -> m (Step s r)) -> r -> m r
+    iterr :: Monad m => LoopI n m a -> (forall s. Step s a -> r -> m (Step s r)) -> r -> m r
 
-instance Unroll 'F where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Flat step s) ->
-        let nfoldlM_F_loop !spec t y = do
-                r <- step t
-                case r of
-                  Yield a t' -> f y a >>= nfoldlM_F_loop spec t'
-                  Skip t' -> nfoldlM_F_loop spec t' y
+instance Iter 'F where
+    {-# INLINE iterl #-}
+    iterl = \(Flat step s) cont ->
+        let iterl_F_loop !spec t = \y -> do
+                q <- step t >>= cont y
+                case q of
+                  Yield z u -> iterl_F_loop spec u z
+                  Skip u -> iterl_F_loop spec u y
                   Done -> return y
-        in nfoldlM_F_loop SPEC s z
+        in iterl_F_loop SPEC s
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Flat step s) ->
-        let nfoldlM'_F_loop !spec t !y = do
-                r <- step t
-                case r of
-                  Yield a t' -> f y a >>= nfoldlM'_F_loop spec t'
-                  Skip t' -> nfoldlM'_F_loop spec t' y
+    {-# INLINE iterl' #-}
+    iterl' = \(Flat step s) cont ->
+        let iterl'_F_loop !spec t = \ !y -> do
+                q <- step t >>= cont y
+                case q of
+                  Yield !z u -> iterl'_F_loop spec u z
+                  Skip u -> iterl'_F_loop spec u y
                   Done -> return y
-        in nfoldlM'_F_loop SPEC s z
+        in iterl'_F_loop SPEC s
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Flat step s) ->
-        let nfoldrM_F_loop !spec t = do
-                r <- step t
-                case r of
-                  Yield a t' -> f a =<< nfoldrM_F_loop spec t'
-                  Skip t' -> nfoldrM_F_loop spec t'
-                  Done -> return z
-        in nfoldrM_F_loop SPEC s
+    {-# INLINE iterr #-}
+    iterr = \(Flat step s) cont ->
+        let iterr_F_loop !spec t = \y -> do
+                q <- step t >>= \a -> cont a y
+                case q of
+                  Yield z u -> iterr_F_loop spec u z
+                  Skip u -> iterr_F_loop spec u y
+                  Done -> return y
+        in iterr_F_loop SPEC s
 
-instance Unroll i => Unroll ('M i) where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Map g l) -> nfoldlM (\y a -> f y (g a)) z l
+instance Iter i => Iter ('M i) where
+    {-# INLINE iterl #-}
+    iterl = \(Map m i) cont -> iterl i $ \r a -> m a >>= cont r
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Map g l) -> nfoldlM' (\ !y a -> f y (g a)) z l
+    {-# INLINE iterl' #-}
+    iterl' = \(Map m i) cont -> iterl' i $ \ !r a -> m a >>= cont r
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Map g l) -> nfoldrM (\a y -> f (g a) y) z l
+    {-# INLINE iterr #-}
+    iterr = \(Map m i) cont -> iterr i $ \a r -> m a >>= \b -> cont b r
 
-instance Unroll 'P where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Pure a) -> a >>= f z
+instance Iter 'P where
+    {-# INLINE iterl #-}
+    iterl = \(Pure ma) cont y -> do
+        a <- ma
+        r <- cont y (Yield a ())
+        case r of
+          Yield z () -> return z
+          Skip () -> return y
+          Done -> return y
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Pure a) -> a >>= f z
+    {-# INLINE iterl' #-}
+    iterl' = \(Pure ma) cont y -> do
+        a <- ma
+        r <- cont y (Yield a ())
+        case r of
+          Yield z () -> return z
+          Skip () -> return y
+          Done -> return y
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Pure a) -> a >>= \b -> f b z
+    {-# INLINE iterr #-}
+    iterr = \(Pure ma) cont y -> do
+        a <- ma
+        r <- cont (Yield a ()) y
+        case r of
+          Yield z () -> return z
+          Skip () -> return y
+          Done -> return y
 
-instance (Unroll i, Unroll j) => Unroll ('A i j) where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Ap gs as) ->
-        nfoldlM (\y g -> nfoldlM (\x a -> f x (g a)) y as) z gs
+instance (Iter i, Iter j) => Iter ('A i j) where
+    {-# INLINE iterl #-}
+    iterl = \(Ap fs as) cont ->
+        iterl fs $ \y -> \case
+          Yield f s -> do
+            z <- iterl (Map (return . fmap f) as) cont y
+            return $ Yield z s
+          Skip s -> return $ Skip s
+          Done -> return Done
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Ap gs as) ->
-        nfoldlM' (\ !y g -> nfoldlM' (\ !x a -> f x (g a)) y as) z gs
+    {-# INLINE iterl' #-}
+    iterl' = \(Ap fs as) cont ->
+        iterl' fs $ \ !y -> \case
+          Yield f s -> do
+            !z <- iterl' (Map (return . fmap f) as) cont y
+            return $ Yield z s
+          Skip s -> return $ Skip s
+          Done -> return Done
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Ap gs as) ->
-        nfoldrM (\g y -> nfoldrM (\a x -> f (g a) x) y as) z gs
+    {-# INLINE iterr #-}
+    iterr = \(Ap fs as) cont ->
+        iterr fs $ \case
+          Yield f s -> \y -> do
+            z <- iterr (Map (return . fmap f) as) cont y
+            return $ Yield z s
+          Skip s -> \_ -> return $ Skip s
+          Done -> \_ -> return Done
 
-instance Unroll i => Unroll ('B i) where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Bind as g) ->
-        nfoldlM (\y a -> foldlM f y (g a)) z as
+instance Iter i => Iter ('B i) where
+    {-# INLINE iterl #-}
+    iterl = \(Bind as f) cont ->
+        iterl as $ \y -> \case
+          Yield a s ->
+            case f a of
+              Loop l -> do
+                z <- iterl l cont y
+                return $ Yield z s
+          Skip s -> return $ Skip s
+          Done -> return Done
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Bind as g) ->
-        nfoldlM' (\ !y a -> foldlM' f y (g a)) z as
+    {-# INLINE iterl' #-}
+    iterl' = \(Bind as f) cont ->
+        iterl' as $ \ !y -> \case
+          Yield a s ->
+            case f a of
+              Loop l -> do
+                !z <- iterl l cont y
+                return $ Yield z s
+          Skip s -> return $ Skip s
+          Done -> return Done
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Bind as g) ->
-        nfoldrM (\a y -> foldrM f y (g a)) z as
+    {-# INLINE iterr #-}
+    iterr = \(Bind as f) cont ->
+        iterr as $ \case
+          Yield a s ->
+            case f a of
+              Loop l -> \y -> do
+                z <- iterr l cont y
+                return $ Yield z s
+          Skip s -> \_ -> return $ Skip s
+          Done -> \_ -> return Done
 
-instance (Unroll i, Unroll j) => Unroll ('S i j) where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \f z (Alt l r) -> nfoldlM f z l >>= (\y -> nfoldlM f y r)
+instance (Iter i, Iter j) => Iter ('S i j) where
+    {-# INLINE iterl #-}
+    iterl = \(Alt i j) cont z -> iterl i cont z >>= iterl j cont
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \f z (Alt l r) -> nfoldlM' f z l >>= (\y -> nfoldlM' f y r)
+    {-# INLINE iterl' #-}
+    iterl' = \(Alt i j) cont z -> iterl' i cont z >>= iterl' j cont
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \f z (Alt l r) -> (\y -> nfoldrM f y l) =<< (nfoldrM f z r)
+    {-# INLINE iterr #-}
+    iterr = \(Alt i j) cont z -> iterr i cont =<< iterr j cont z
 
-instance Unroll 'Z where
-    {-# INLINE nfoldlM #-}
-    nfoldlM = \_ z Zero -> return z
+instance Iter 'Z where
+    {-# INLINE iterl #-}
+    iterl = \Zero cont y -> do
+        r <- cont y Done
+        case r of
+          Yield z _ -> return z
+          Skip _ -> return y
+          Done -> return y
 
-    {-# INLINE nfoldlM' #-}
-    nfoldlM' = \_ z Zero -> return z
+    {-# INLINE iterl' #-}
+    iterl' = \Zero cont y -> do
+        r <- cont y Done
+        case r of
+          Yield z _ -> return z
+          Skip _ -> return y
+          Done -> return y
 
-    {-# INLINE nfoldrM #-}
-    nfoldrM = \_ z Zero -> return z
+    {-# INLINE iterr #-}
+    iterr = \Zero cont y -> do
+        r <- cont Done y
+        case r of
+          Yield z _ -> return z
+          Skip _ -> return y
+          Done -> return y
 
-data Loop m a = forall (n :: LS). Unroll n => Loop (LoopI n m a)
+nfoldlM :: (Iter n, Monad m) => (a -> b -> m a) -> a -> LoopI n m b -> m a
+{-# INLINE nfoldlM #-}
+nfoldlM = \f x l ->
+  iterl l (\y -> \case
+    Yield a s -> f y a >>= \z -> return $ Yield z s
+    Skip s -> return $ Skip s
+    Done -> return Done) x
 
-instance Functor m => Functor (Loop m) where
+nfoldlM' :: (Iter n, Monad m) => (a -> b -> m a) -> a -> LoopI n m b -> m a
+{-# INLINE nfoldlM' #-}
+nfoldlM' = \f x l ->
+  iterl' l (\y -> \case
+    Yield a s -> f y a >>= \z -> return $ Yield z s
+    Skip s -> return $ Skip s
+    Done -> return Done) x
+
+nfoldrM :: (Iter n, Monad m) => (a -> b -> m b) -> b -> LoopI n m a -> m b
+{-# INLINE nfoldrM #-}
+nfoldrM = \f x l ->
+  iterr l (\case
+    Yield a s -> \y -> f a y >>= \z -> return $ Yield z s
+    Skip s -> \_ -> return $ Skip s
+    Done -> \_ -> return Done) x
+
+data Loop m a = forall (n :: LS). Iter n => Loop (LoopI n m a)
+
+instance Applicative m => Functor (Loop m) where
     {-# INLINE fmap #-}
-    fmap = \f (Loop l) -> Loop (Map f l)
+    fmap = \f (Loop l) -> Loop (Map (pure . fmap f) l)
 
 instance Applicative m => Applicative (Loop m) where
     {-# INLINE pure #-}
